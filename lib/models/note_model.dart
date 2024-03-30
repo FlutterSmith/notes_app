a6bf085f36aadd1c0ee2a24f2face6bcc043548e
Your Name <you@example.com>
2024-03-30 03:01:43 +0200

    add hive

--- lib/models/note_model.dart
+++ lib/models/note_model.dart
@@ -4,11 +4,18 @@ class NoteModel {
   DateTime dateTime = DateTime.now();
   final int color;
 
-  
   NoteModel({
     required this.title,
     required this.content,
     required this.dateTime,
     required this.color,
   });
+  factory NoteModel.fromJson(Map<String, dynamic> json) {
+    return NoteModel(
+      title: json['title'],
+      content: json['content'],
+      dateTime: DateTime.parse(json['dateTime']),
+      color: json['color'],
+    );
+  }
 }

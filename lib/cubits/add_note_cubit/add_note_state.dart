fe1796251eb7f3b647ef460cfc62ad0a856c4ae2
Your Name <you@example.com>
2024-03-31 10:29:45 +0200

    added add note states

--- lib/cubits/add_note_cubit/add_note_state.dart
+++ lib/cubits/add_note_cubit/add_note_state.dart
@@ -3,4 +3,21 @@ part of 'add_note_cubit.dart';
 @immutable
 sealed class AddNoteState {}
 
-final class AddNoteInitial extends AddNoteState {}
+final class AddNoteInitial extends AddNoteState {
+
+}
+
+final class AddNoteLoading extends AddNoteState {
+
+}
+
+final class AddNoteSuccess extends AddNoteState {
+   
+}
+
+final class AddNoteFailure extends AddNoteState {
+  final String errMessage;
+
+  AddNoteFailure(this.errMessage);
+}
+
